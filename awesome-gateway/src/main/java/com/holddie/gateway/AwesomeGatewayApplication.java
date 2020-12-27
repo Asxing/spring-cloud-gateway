8c6413d7a7d7d1f27b0fa31cb33e00a508d8fd4f
holddie <holddie@163.com>
2020-12-28 00:13:41 +0800

    fix: Add the base demo and skip the unit tests.

---
+++ awesome-gateway/src/main/java/com/holddie/gateway/AwesomeGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.holddie.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AwesomeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("path_route", r -> r.path("/get")
+						.uri("http://httpbin.org"))
+				.build();
+	}
+}
