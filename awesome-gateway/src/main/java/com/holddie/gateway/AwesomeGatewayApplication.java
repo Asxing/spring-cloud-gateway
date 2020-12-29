8f8b7a0f26226b22d6369cc2ec47d3698e9468d2
holddie <holddie@163.com>
2020-12-29 11:20:23 +0800

    fix: 拦截指定host错误fallback.

--- awesome-gateway/src/main/java/com/holddie/gateway/AwesomeGatewayApplication.java
+++ awesome-gateway/src/main/java/com/holddie/gateway/AwesomeGatewayApplication.java
@@ -1,11 +1,16 @@
 package com.holddie.gateway;
 
+import reactor.core.publisher.Mono;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AwesomeGatewayApplication {
 
@@ -16,12 +21,24 @@ public class AwesomeGatewayApplication {
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes().
-				route("path_route", r -> r.path("/get")
+				route("path_route", r -> r
+						.path("/get")
 						.filters(f -> f.addRequestHeader("Hello", "World"))
 						.uri("http://httpbin.org")).
-				route("hystrix_route", r -> r.host("*.hystrix.com")
-						.filters(f -> f.hystrix(config -> config.setName("mycmd")))
+				route("hystrix_route", r -> r
+						.host("*.hystrix.com")
+						.filters(f ->
+								f.hystrix(config -> config
+										.setName("mycmd")
+										.setFallbackUri("forward:/fallback")
+								)
+						)
 						.uri("http://httpbin.org:80"))
 				.build();
+	}
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
 	}
 }
